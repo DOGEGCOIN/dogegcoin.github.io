5e94f1cf5dc1d95cf35ec144d5a8598078da2254
DOGEGCOIN <support@dogegcoin>
2021-12-27 23:14:25 +0700

    fix some minor bugs

--- js/app3.js
+++ js/app3.js
@@ -509,7 +509,7 @@ function getreflink() {
     if (!/^(0x){1}[0-9a-fA-F]{40}$/i.test(referaladd)) {
       Swal.fire('Referral Alert', 'Your address is not valid.', 'error')
     } else {
-      document.getElementById('refaddress').value = 'https://shibgtoken.com/?ref=' + document.getElementById('refaddress').value;
+      document.getElementById('refaddress').value = 'https://dogegcoin.com/?ref=' + document.getElementById('refaddress').value;
     }
   }
 }
